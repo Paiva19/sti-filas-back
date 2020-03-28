58122bbe053df7f8c2e64290e14a431f844353fb
lucas paiva <lucas.gomes.de.paiva@usp.br>
2020-03-27 22:52:22 -0300

    Add port to be deployed

--- sti_app_back.py
+++ sti_app_back.py
@@ -440,4 +440,4 @@ def cupons_por_fila(idAtracao):
 
 ############################################################################################################
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="33")
